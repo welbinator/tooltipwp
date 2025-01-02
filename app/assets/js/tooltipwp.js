0ae13019983fedac1826bfd3ccec3f8abb27df23
James <james.welbes@gmail.com>
2025-01-01 21:17:27 -0600

    fix before and after

--- app/assets/js/tooltipwp.js
+++ app/assets/js/tooltipwp.js
@@ -4,17 +4,20 @@ jQuery(document).ready(function($) {
         console.log('Processing tooltip:', tooltip);
         $('.' + tooltip.class).each(function() {
             $(this).addClass('tooltipwp_tooltip');
-            // $(this).attr('data-tooltip', tooltip.text);
 
             // Create an <i> element with the Font Awesome class
             var iconClass = 'tooltip-icon ' + tooltip.icon;
             var iconElement = $('<i>').addClass(iconClass);
 
-            // Append the <i> element after the tooltip element
-            $(this).append(iconElement);
-            iconElement.attr('data-tooltip', tooltip.text);
+            // Append the <i> element in the appropriate position based on tooltip.position
+            if (tooltip.position === 'before') {
+                $(this).prepend(iconElement); // Adds the tooltip before the element
+            } else {
+                $(this).append(iconElement); // Adds the tooltip after the element
+            }
 
-            console.log('Element:', this, 'Data-tooltip:', tooltip.text, 'Icon class:', iconClass);
+            iconElement.attr('data-tooltip', tooltip.text);
+            console.log('Element:', this, 'Data-tooltip:', tooltip.text, 'Icon class:', iconClass, 'Position:', tooltip.position);
         });
     });
 });
